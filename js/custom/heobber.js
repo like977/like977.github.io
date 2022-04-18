ff566d79a28454659dbc1968effb09d520a55379
liam <1768045871@qq.com>
2022-04-18 08:50:45 +0800

    来自PC-Liam更新推送！

--- js/custom/heobber.js
+++ js/custom/heobber.js
@@ -1,1 +1,100 @@
-function getbbdata(){var bbsurl=bbapiurl,httpRequest=new XMLHttpRequest;httpRequest.open("GET",bbsurl,!0),httpRequest.send(),httpRequest.onreadystatechange=function(){if(4==httpRequest.readyState&&200==httpRequest.status){var json=httpRequest.responseText,obj=eval("("+json+")");const bbArray=obj.data.map((e=>({date:e.date,content:e.content,from:e.from})));saveToLocal.set("zhheo-bb",JSON.stringify(bbArray),5/1440);const data=saveToLocal.get("zhheo-bb");generateBBHtml(JSON.parse(data))}}}var generateBBHtml=e=>{var t=document.querySelector("#bber"),a='<section class="timeline page-1"><ul><div class="list">';if(console.log(e),e.length)for(let t=0;t<e.length;t++){var r="";r=-1!=e[t].from.indexOf("iPhone")?'<i class="fas fa-mobile-alt"></i>':-1!=e[t].from.indexOf("iPad")?'<i class="fas fa-tablet-alt"></i>':-1!=e[t].from.indexOf("Mac")?'<i class="fas fa-laptop"></i>':-1!=e[t].from.indexOf("微信")?'<i class="fab fa-weixin" style="font-size: 0.6rem"></i>':'<i class="fas fa-tools"></i>';var o=new Date(e[t].date);e[t].date;a+='<li class="item"><div>'+('<p class="datatime">'+(o.getFullYear()+"/"+(o.getMonth()+1)+"/"+o.getDate()+" "+o.getHours()+":"+o.getMinutes()+":"+o.getSeconds())+"</p>")+('<p class="datacont">'+urlToLink(e[t].content)+"</p>")+'</p><p class="datafrom"><small>'+r+e[t].from+"</small></p></div></li>"}else a+='!{_p("aside.card_funds.zero")}';a+="</div></ul></section>",(t=document.querySelector("#bber")).innerHTML=a,Lately({target:"#bber .datatime"}),window.lazyLoadInstance&&window.lazyLoadInstance.update(),window.pjax&&window.pjax.refresh(t)};function urlToLink(e){return e=(e=e.replace(/\bhttps?:[^:<>"]*\/([^:<>"]*)(\.(jpeg)|(png)|(jpg)|(webp))(!blogimg)?/g,(function(e){return'<a href="'+e+'"><img src="'+e+'" /></a>'}))).replace(/\bhttps?:\/\/(?!\S+(?:jpe?g|png|bmp|gif|webp|gif))\S+/g,(function(e){return" <a href='"+e+"'rel='noopener' target='_blank'>↘链接↙</a> "}))}document.querySelector("#bber")&&getbbdata();var $jscomp=$jscomp||{};$jscomp.scope={},$jscomp.arrayIteratorImpl=function(e){var t=0;return function(){return t<e.length?{done:!1,value:e[t++]}:{done:!0}}},$jscomp.arrayIterator=function(e){return{next:$jscomp.arrayIteratorImpl(e)}},$jscomp.makeIterator=function(e){var t="undefined"!=typeof Symbol&&Symbol.iterator&&e[Symbol.iterator];return t?t.call(e):$jscomp.arrayIterator(e)},function(e,t){var a=function(){return this||(0,eval)("this")}();"Lately"in a||(a.Lately=function(e){for(var t=e.lang||{second:"秒",minute:"分钟",hour:"小时",day:"天",month:"个月",year:"年",ago:"前",error:"NaN"},a=(e=$jscomp.makeIterator(document.querySelectorAll(e.target||".time"))).next();!a.done;a=e.next()){var r=(a=a.value).dateTime,o=a.title,n=a.innerHTML;if(!r||isNaN(new Date(r=r.replace(/(.*)[a-z](.*)\+(.*)/gi,"$1 $2").replace(/-/g,"/"))))if(o&&!isNaN(new Date(o=o.replace(/-/g,"/"))))r=o;else{if(!n||isNaN(new Date(n=n.replace(/-/g,"/"))))break;r=n}a.title=r,r=new Date(r);var s=(n=(o=(r=((new Date).getTime()-r.getTime())/1e3)/60)/60)/24,i=s/30,l=i/12;a.innerHTML=(1<=l?Math.floor(l)+t.year:1<=i?Math.floor(i)+t.month:1<=s?Math.floor(s)+t.day:1<=n?Math.floor(n)+t.hour:1<=o?Math.floor(o)+t.minute:1<=r?Math.floor(r)+t.second:t.error)+t.ago}})}();
+// if (document.querySelector('#bber-loading')) {
+//   var loading_pic = document.getElementById('bber-loading');
+//   loading_pic.innerHTML = '<span id="moments_loading"><i class="fa fa-spinner fa-spin"></i></span>';
+// }
+
+function getbbdata(){
+    var bbsurl = bbapiurl
+    
+    var httpRequest = new XMLHttpRequest();//第一步：建立所需的对象
+    httpRequest.open('GET', bbsurl, true);//第二步：打开连接  将请求参数写在url中  ps:"./Ptest.php?name=test&nameone=testone"
+    httpRequest.send();//第三步：发送请求  将请求参数写在URL中
+    /**
+     * 获取数据后的处理程序
+     */
+    httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+        var json = httpRequest.responseText;//获取到json字符串，还需解析
+        var obj = eval('(' + json + ')');
+        // console.log(obj.data)
+        const bbArray = obj.data.map(e => {
+        return {
+            'date': e.date,
+            'content': e.content,
+            'from': e.from
+        }
+        })
+        // console.log(fundsArray)
+        saveToLocal.set('zhheo-bb', JSON.stringify(bbArray), 5 / (60 * 24))
+        const data = saveToLocal.get('zhheo-bb');
+        generateBBHtml(JSON.parse(data))
+    }
+    };
+  }
+  
+  var generateBBHtml = array => {
+    var $dom = document.querySelector('#bber');
+    var result = '<section class="timeline page-1"><ul><div class="list">'
+    console.log(array)
+  
+    if (array.length) {
+      for (let i = 0; i < array.length; i++) {
+        var from_icon = '';
+            if (array[i].from.indexOf("iPhone") != -1){
+              from_icon = '<i class="fas fa-mobile-alt"></i>';
+            }else if (array[i].from.indexOf("iPad") != -1){
+              from_icon = '<i class="fas fa-tablet-alt"></i>';
+            }else if (array[i].from.indexOf("Mac") != -1){
+              from_icon = '<i class="fas fa-laptop"></i>';
+            }else if (array[i].from.indexOf("微信") != -1){
+              from_icon = '<i class="fab fa-weixin" style="font-size: 0.6rem"></i>';
+            }else{
+              from_icon = '<i class="fas fa-tools"></i>';
+            };
+  
+        var d = new Date(array[i].date);
+        var dtime = array[i].date,data = d.getFullYear()+'/'+(d.getMonth()+1)+'/'+d.getDate() +' '+d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
+        var dataCont = '<p class="datacont">'+urlToLink(array[i].content)+'</p>';
+        var dataTime = '<p class="datatime">'+data+'</p>';
+  
+  
+        result += `<li class="item"><div>`+ dataTime  + dataCont +`</p><p class="datafrom"><small>`+ from_icon + array[i].from +`</small></p></div></li>`;
+      }
+    } else {
+      result += '!{_p("aside.card_funds.zero")}';
+    }
+    result += '</div></ul></section>'
+    
+    var $dom = document.querySelector('#bber');
+    
+    $dom.innerHTML = result;
+    Lately({ 'target': '#bber .datatime' });
+    window.lazyLoadInstance && window.lazyLoadInstance.update();
+    window.pjax && window.pjax.refresh($dom);
+  }
+  
+  if (document.querySelector('#bber')) {
+    getbbdata()
+  }
+  
+  function urlToLink(str) {
+  var re =/\bhttps?:\/\/(?!\S+(?:jpe?g|png|bmp|gif|webp|gif))\S+/g;
+  var re_forpic =/\bhttps?:[^:<>"]*\/([^:<>"]*)(\.(jpeg)|(png)|(jpg)|(webp))(!blogimg)?/g;
+  str =str.replace(re_forpic,function (imgurl) {
+    return '<a href="' + imgurl + '"><img src="' + imgurl + '" /></a>';
+  });
+  str =str.replace(re,function (website) {
+    return " <a href='" + website + "'rel='noopener' target='_blank'>↘链接↙</a> ";
+  });
+  // str = qqWechatEmotionParser(str)
+  return str; 
+  }
+  /*
+  MIT License - http://www.opensource.org/licenses/mit-license.php
+  For usage and examples, visit:
+  https://tokinx.github.io/lately/
+  Copyright (c) 2017, Biji.IO
+  */
+  var $jscomp=$jscomp||{};$jscomp.scope={};$jscomp.arrayIteratorImpl=function(b){var g=0;return function(){return g<b.length?{done:!1,value:b[g++]}:{done:!0}}};$jscomp.arrayIterator=function(b){return{next:$jscomp.arrayIteratorImpl(b)}};$jscomp.makeIterator=function(b){var g="undefined"!=typeof Symbol&&Symbol.iterator&&b[Symbol.iterator];return g?g.call(b):$jscomp.arrayIterator(b)};
+  (function(b,g){var p=function(h){var d=h.lang||{second:"\u79d2",minute:"\u5206\u949f",hour:"\u5c0f\u65f6",day:"\u5929",month:"\u4e2a\u6708",year:"\u5e74",ago:"\u524d",error:"NaN"};h=$jscomp.makeIterator(document.querySelectorAll(h.target||".time"));for(var c=h.next();!c.done;c=h.next()){c=c.value;var a=c.dateTime;var e=c.title,f=c.innerHTML;if(!a||isNaN(new Date(a=a.replace(/(.*)[a-z](.*)\+(.*)/gi,"$1 $2").replace(/-/g,"/"))))if(e&&!isNaN(new Date(e=e.replace(/-/g,"/"))))a=e;else if(f&&!isNaN(new Date(f=
+  f.replace(/-/g,"/"))))a=f;else break;c.title=a;a=new Date(a);a=((new Date).getTime()-a.getTime())/1E3;e=a/60;f=e/60;var k=f/24,l=k/30,m=l/12;c.innerHTML=(1<=m?Math.floor(m)+d.year:1<=l?Math.floor(l)+d.month:1<=k?Math.floor(k)+d.day:1<=f?Math.floor(f)+d.hour:1<=e?Math.floor(e)+d.minute:1<=a?Math.floor(a)+d.second:d.error)+d.ago}};var n=function(){return this||(0,eval)("this")}();"Lately"in n||(n.Lately=p)})();
